f524663d2cd9ffebd592ea3375f85e8113c50343
VamoCodar <adr@Acer.localdomain>
2023-07-13 13:14:25 -0300

    resolve

--- static/dist/global-084699f7.js
+++ static/dist/global-084699f7.js
@@ -1,1 +1,2 @@
 const t={screen:{md:()=>window.matchMedia("(max-width: 1180px)").matches},context:{}};window.GLOBAL=t;
+//# sourceMappingURL=global-084699f7.js.map
---
+++ static/dist/index-1d3f78a9.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>document.body.classList.add("DOMContentLoaded"));
+//# sourceMappingURL=index-1d3f78a9.js.map
