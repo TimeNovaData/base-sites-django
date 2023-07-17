5e1954652ca018fe7a09b018ec9219fcfac6e810
Clara Moreira <anacacamm@gmail.com>
2023-07-17 17:07:40 -0300

    criando algumas seções de serviços

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,33 +1,27 @@
-import axios from "axios";
-import axiosConfig from "./modules/axiosConfig";
+import axios from 'axios'
+import axiosConfig from './modules/axiosConfig'
 import PageHome from './pages/Home.js'
-import UseMouseOrKeyboard from "./modules/UseMouseOrKeyboard";
-import DesignSystem from "./DesignSystem/DesignSystem";
-import { register } from 'swiper/element/bundle';
+import UseMouseOrKeyboard from './modules/UseMouseOrKeyboard'
+import DesignSystem from './DesignSystem/DesignSystem'
+import { register } from 'swiper/element/bundle'
+
+import showCardContent from './modules/showCardContent'
 
 // import SvgUse from "./utils/SvgUse";
 
 // Init
 GLOBAL.DesignSystem = DesignSystem({})
-register() // Swiper Web Component 
+register() // Swiper Web Component
 
 // Config
 axiosConfig({ csrf: GLOBAL.context?.csrf }, axios)
 UseMouseOrKeyboard()
 
+showCardContent()
+
 // Pages
 PageHome()
-
-
-
-
-
-
-
 
 if (import.meta.env.DEV) {
 	console.log('\x1b[33mGLOBAL\n', GLOBAL)
 }
-
-
-
