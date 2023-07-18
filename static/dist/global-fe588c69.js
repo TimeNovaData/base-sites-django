ce03db075b3b8d8a01a227f43d3e4fff9843ece5
VamoCodar <adr@Acer.localdomain>
2023-07-17 23:15:00 -0300

    resolvendo conflitos

---
+++ static/dist/global-fe588c69.js
@@ -0,0 +1,2 @@
+const t={screen:{md:()=>window.matchMedia("(max-width: 1024px)").matches,sm:()=>window.matchMedia("(max-width: 600px)").matches},context:{}};window.GLOBAL=t;
+//# sourceMappingURL=global-fe588c69.js.map
---
+++ static/dist/index-b95bf1fb.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>document.body.classList.add("DOMContentLoaded"));
+//# sourceMappingURL=index-b95bf1fb.js.map
