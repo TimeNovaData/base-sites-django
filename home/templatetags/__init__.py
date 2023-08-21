659f8474aea580da72156f6b619e95fe49554e4a
paulinaJulia <paulinajulia2015@gmail.com>
2023-08-21 10:24:00 -0300

    alterando tags

--- home/templatetags/__init__.py
+++ home/templatetags/__init__.py
@@ -1,3 +1,3 @@
-from home.templatetags import django_vite_tags, load_colors
+from home.templatetags import load_colors, ndt_vite_tags
 
-__all__ = [load_colors, django_vite_tags]
+__all__ = [load_colors, ndt_vite_tags]
