cb8b8a9504652d9b83235cb6fa18798340d61cbe
lopesjorge <jorgelopeslopes@outlook.com>
2023-10-31 11:44:19 -0300

    first commit

--- lubrisint/settings.py
+++ lubrisint/settings.py
@@ -13,7 +13,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "seo_pro.herokuapp.com",
+    "lubrisint.herokuapp.com",
     "seo-pro.dokku-sites.novadata.com.br",
 ]
 
@@ -74,7 +74,7 @@ DEV = config("DEV", default=False, cast=bool)
 #         # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 #     ]
 
-ROOT_URLCONF = "seo_pro.urls"
+ROOT_URLCONF = "lubrisint.urls"
 
 TEMPLATES = [
     {
@@ -92,7 +92,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "seo_pro.wsgi.application"
+WSGI_APPLICATION = "lubrisint.wsgi.application"
 
 
 default_dburl = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
@@ -123,7 +123,7 @@ if USE_AWS:
         AWS_S3_CUSTOM_DOMAIN,
         PUBLIC_MEDIA_LOCATION,
     )
-    DEFAULT_FILE_STORAGE = "seo_pro.storages_backends.PublicMediaStorage"
+    DEFAULT_FILE_STORAGE = "lubrisint.storages_backends.PublicMediaStorage"
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "seo_pro API",
-    "DESCRIPTION": "seo_pro description",
+    "TITLE": "lubrisint API",
+    "DESCRIPTION": "lubrisint description",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     #
