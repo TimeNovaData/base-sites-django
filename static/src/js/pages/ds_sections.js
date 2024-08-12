57f9209c5394fb3d8a6ea320ed6c8e88365699cc
lopesjorge <jorgelopeslopes@outlook.com>
2024-08-12 16:51:26 -0300

    ajustando e criando novas sections gallery

--- static/src/js/pages/ds_sections.js
+++ static/src/js/pages/ds_sections.js
@@ -1,10 +1,15 @@
 import Swiper from "swiper"
 import { find } from "utils/dom"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 export default function () {
 	const Sections = find("[js-page='ds-sections']")
 	if (!Sections) return
 	console.log("Page DS Sections");
+
+	/* Todos os swippers do DS sections about e blog*/
 
 	const swiper5 = new Swiper('.swiper-about5', {
 		slidesPerView: 'auto',
@@ -202,5 +207,255 @@ export default function () {
 			dynamicBullets: false,
 			dynamicMainBullets: 3,
 		}
+	});
+
+	const swiperStage6 = new Swiper('.swiper-stage6', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		navigation: {
+			nextEl: '.swiper-stage6-next',
+			prevEl: '.swiper-stage6-prev',
+		}
+	});
+
+	/* Time line acompanhando o scroll 
+		Ver exemplo em section stage 8
+	*/
+	function timeline(){
+		const tl = gsap.timeline({
+			scrollTrigger: {
+			  trigger: '.our-story-pinss',
+				 scrub: 1,
+				start: "top 50%",
+				end: "bottom 50%",      
+			  }
+		  })
+		  tl.to('.back', { height: '100%'})
+	  
+		  gsap.from('.our-story-pinss .our-story-1', {
+			opacity: 0,
+			y: 20,
+			scrollTrigger: {
+			  trigger: '.our-story-pinss .our-story-1',
+				start: "top 60%",      
+			  }
+	  
+		  })
+
+		  gsap.from('.our-story-pinss .our-story-2', {
+			opacity: 0,
+			y: 20,
+			scrollTrigger: {
+			  trigger: '.our-story-pinss .our-story-2',       
+				start: "top 60%",   
+			  }
+	  
+		  })
+	}
+  
+	timeline()
+
+	/* Todos os swippers do DS sections Gallery */
+	const swiperGallery1 = new Swiper('.swiper-gallery1', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		navigation: {
+			nextEl: '.swiper-gallery1-next',
+			prevEl: '.swiper-gallery1-prev',
+		},
+
+		pagination: {
+			el: '.swiper-pagination-gallery1',
+			clickable: true,
+			dynamicBullets: false,
+			dynamicMainBullets: 3,
+		}
+	});
+
+	const swiperGallery2 = new Swiper('.swiper-gallery2', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		navigation: {
+			nextEl: '.swiper-gallery2-next',
+			prevEl: '.swiper-gallery2-prev',
+		},
+
+		pagination: {
+			el: '.swiper-pagination-gallery2',
+			clickable: true,
+			dynamicBullets: false,
+			dynamicMainBullets: 3,
+		}
+	});
+
+	const swiperGallery3 = new Swiper('.swiper-gallery3', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		navigation: {
+			nextEl: '.swiper-gallery3-next',
+			prevEl: '.swiper-gallery3-prev',
+		},
+
+		pagination: {
+			el: '.swiper-pagination-gallery3',
+			clickable: true,
+			dynamicBullets: false,
+			dynamicMainBullets: 3,
+		},
+		breakpoints:{
+			600:{
+				spaceBetween:32,
+			}
+		}
+	});
+
+	const swiperGallery4 = new Swiper('.swiper-gallery4', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		
+		navigation: {
+			nextEl: '.swiper-gallery4-next',
+			prevEl: '.swiper-gallery4-prev',
+		},
+
+		pagination: {
+			el: '.swiper-pagination-gallery4',
+			clickable: true,
+			dynamicBullets: false,
+			dynamicMainBullets: 3,
+		},
+		breakpoints:{
+			600:{
+				spaceBetween:32,
+			},
+
+			1024:{
+				slidesPerView:4,
+			}
+		},
+		// Configurações do Swiper
+		on: {
+			slideChange: function () {
+			const activeSlide = swiperGallery4.slides[swiperGallery4.activeIndex];
+			const divWrap = activeSlide.querySelector('.item-slide-gallery')
+			const divWrapAll = document.querySelectorAll('.item-slide-gallery')
+			divWrapAll.forEach(i=> i.classList.remove('active'))
+			divWrap.classList.add('active')
+			console.log("slide active", activeSlide);
+			}
+		}
+		
+	});
+
+	const swiperGallery6 = new Swiper('.swiper-gallery6', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		loop:true,
+		autoplay: {
+			delay: 1000,
+			disableOnInteraction: false,
+		  },
+	});
+
+	const swiperGallery6Bottom = new Swiper('.swiper-gallery6-bottom', {
+		slidesPerView: 'auto',
+		spaceBetween:16,
+		loop:true,
+		autoplay: {
+			delay: 1000,
+			disableOnInteraction: false,
+		  },
+	});
+
+	setInterval(() => {
+		swiperGallery6Bottom.slidePrev(); // Mover para o slide anterior
+	  }, 1000); 
+
+	  /**
+	    Animation gallery 7
+	   */
+	 function scrollGsapImg() {
+		if (window.matchMedia("(max-width: 1100px)").matches) return
+	
+		const cardsWrapper = document.querySelector("#gallery_7 .cards-wrapper");
+			if (!cardsWrapper) return
+			const col1 = cardsWrapper.querySelector(".col-1"),
+			col2 = cardsWrapper.querySelector(".col-2"),
+			col3 = cardsWrapper.querySelector(".col-3"),
+			col4 = cardsWrapper.querySelector(".col-4"),
+			col5 = cardsWrapper.querySelector(".col-5");
+			// col6 = cardsWrapper.querySelector(".col-6");
+	
+		const value = 200, duration = 40;
+	
+		gsap.set(col1, {
+			y: - value
+		}, "juntas")
+		gsap.set(col2, {
+			y: value
+		}, "juntas")
+		gsap.set(col3, {
+			y: - value
+		}, "juntas")
+		gsap.set(col4, {
+			y: value
+		}, "juntas")
+		gsap.set(col5, {
+			y: - value
+		}, "juntas")
+		// gsap.set(col6, {
+		// 	y: value
+		// }, "juntas")
+	
+		const tl =
+			gsap
+				.timeline({ paused: true, delay: 0, ease: "linear", repeat: -1, yoyo: true, yoyoEase: false })
+				.to(col1, {
+					duration: duration,
+					y: value
+				}, "juntas")
+				.to(col2, {
+					duration: duration,
+					y: - value
+				}, "juntas")
+				.to(col3, {
+					duration: duration,
+					y: value
+				}, "juntas")
+				.to(col4, {
+					duration: duration,
+					y: - value
+				}, "juntas")
+				.to(col5, {
+					duration: duration,
+					y: value
+				}, "juntas")
+				// .to(col6, {
+				// 	duration: duration,
+				// 	y: - value
+				// }, "juntas")
+	
+		window.tl = tl
+	
+	
+		ScrollTrigger.create({
+			trigger: cardsWrapper,
+			once: false,
+			start: 'top-=100px',
+			refreshPriority: 10,
+			//markers: true,
+			onEnter: function () {
+				tl.play()
+	
+			},
+		});
+	}
+
+	scrollGsapImg()
+
+	/* Todos os swippers do DS sections Gallery */
+	const swiperInstagram1 = new Swiper('.swiper-instagram1', {
+		slidesPerView: 'auto',
+		spaceBetween:8
 	});
 }
--- static/src/js/utils/typography.js
+++ static/src/js/utils/typography.js
@@ -100,6 +100,103 @@ const $type = {
 			},
 		},
 	},
+
+	Headline: {
+		1: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '18px',
+				fontWeight: 500,
+				letterSpacing: '-2%',
+				lineHeight: '24px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+
+		2: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '16px',
+				fontWeight: 500,
+				letterSpacing: '-2%',
+				lineHeight: '24px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+		
+	},
+
+	Paragraph: {
+		1: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '18px',
+				fontWeight: 400,
+				letterSpacing: '-2%',
+				lineHeight: '26px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+
+		2: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '16px',
+				fontWeight: 400,
+				letterSpacing: '-2%',
+				lineHeight: '24px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+
+		3: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '14px',
+				fontWeight: 400,
+				letterSpacing: '-2%',
+				lineHeight: '22px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+
+		4: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '12px',
+				fontWeight: 400,
+				letterSpacing: '-2%',
+				lineHeight: '20px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+
+		5: {
+			$type: 'typography',
+			$value: {
+				fontFamily: 'Space Grotesk',
+				fontSize: '10px',
+				fontWeight: 300,
+				letterSpacing: '-2%',
+				lineHeight: '18px',
+				textTransform: 'none',
+				textDecoration: 'none',
+			},
+		},
+		
+	},
 }
 
 function toRem($value, isPercentage = false) {
